4d0b08931ba8894e2c7e077ebb6defa05050eb73
NicoRoch <sairo74@aol.com>
2015-11-26 15:27:56 +0100

    Include copyright

--- ATS9360/DataTreatment.py
+++ ATS9360/DataTreatment.py
@@ -1,3 +1,21 @@
+# This Python file uses the following encoding: utf-8
+# ATS9360_NPT.py driver for The aquisition board Alzar ATS9360
+# Etienne Dumur <etienne.dumur@neel.cnrs.fr> 2015
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import numpy as np
 import time
 import multiprocessing as mp
